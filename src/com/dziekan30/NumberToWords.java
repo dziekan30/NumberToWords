769a392e24e0c1373d1124d673c2c4a922aae549
dziekan30 <dziekan30@gmail.com>
2022-04-27 09:06:35 -0500

    Number to words

--- src/com/dziekan30/NumberToWords.java
+++ src/com/dziekan30/NumberToWords.java
@@ -1,19 +1,54 @@
-package com.dziekan30;
-
 public class NumberToWords {
-    public static int reverse(int number) {
-        int reverseNumber = 0;
-
-        while (number != 0) {
-            // First you need calculate last digit of the number
-            int lastDigit = number % 10;
-            // If you have last digit you need calculate reverse number. Reverse number was
-            // calculated in this pattern actual reverseNumber = reverseNumber * 10 + last Digit.
-            reverseNumber = (reverseNumber * 10) + lastDigit;
-            //The last step you need to take is to delete the last digit from the number
-            //Simpler way: number = number / 10
+    public static void numberToWords(int number){
+        if(number<0) System.out.print("Invalid Value");
+        int count = getDigitCount(number);
+        number = reverse(number);
+        for(int i=0; i<count; i++){
+            int digit = number%10;
+            switch(digit){
+                case 0:
+                    System.out.print("Zero "); break;
+                case 1:
+                    System.out.print("One "); break;
+                case 2:
+                    System.out.print("Two "); break;
+                case 3:
+                    System.out.print("Three "); break;
+                case 4:
+                    System.out.print("Four "); break;
+                case 5:
+                    System.out.print("Five "); break;
+                case 6:
+                    System.out.print("Six "); break;
+                case 7:
+                    System.out.print("Seven "); break;
+                case 8:
+                    System.out.print("Eight "); break;
+                case 9:
+                    System.out.print("Nine "); break;
+                default: break;
+            }
             number /= 10;
         }
-        return reverseNumber;
+    }
+
+    public static int reverse(int number) {
+        int reverse = 0;
+        int digit = 0;
+        while(number!=0) {
+            reverse = (reverse*10) + number%10;
+            number /= 10;
+        }
+        return reverse;
+    }
+
+    public static int getDigitCount(int number){
+        if (number<0) return -1;
+        int count = 0;
+        do {
+            count++;
+            number /= 10;
+        } while (number>0);
+        return count;
     }
 }
